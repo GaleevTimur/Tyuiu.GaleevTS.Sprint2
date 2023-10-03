302092a675f5e938962d552e2b19eb6bee8fa454
GaleevTimur <timur.galeev.05@yandex.ru>
2023-10-03 23:28:04 +0500

    Add project files.

---
+++ Tyuiu.GaleevTS.Sprint2.Task0.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GaleevTS.Sprint2.Task0.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GaleevTS.Sprint2.Task0.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a5d670c-bc42-43aa-8cd1-bf1251e5df67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GaleevTS.Sprint2.Task0.V30/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GaleevTS.Sprint2.Task0.V30.Lib;
+
+namespace Tyuiu.GaleevTS.Sprint2.Task0.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 95;
+            int y = 1735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+
+
+            Console.Title = "Спринт №1 | Выполнил: Галеев Т. С. | ИИПб-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                 *");
+            Console.WriteLine("* Задание №0                                                               *");
+            Console.WriteLine("* Вариант #30                                                              *");
+            Console.WriteLine("* Выполнил: Галеев Тимур Серикович | ИИПб-23-3                             *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=)          *");
+            Console.WriteLine("* и арифмитических выражений, которая вернет логическую последовательность *");
+            Console.WriteLine("* (True, False, True, False, True, False)                                  *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Значение x = " + x);
+            Console.WriteLine("Значение y = " + y);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            for (int i = 0; i<6; i++)
+            {
+                Console.WriteLine(res[i]);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
